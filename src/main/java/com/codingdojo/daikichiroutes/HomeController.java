c8969bcc95f4c6d89153c5e022fd8cf5dc7603d0
Chris Ventura <cvent820@gmail.com>
2023-07-18 13:48:50 -0700

    initial commit

--- src/main/java/com/codingdojo/daikichiroutes/HomeController.java
+++ src/main/java/com/codingdojo/daikichiroutes/HomeController.java
@@ -24,13 +24,25 @@ public class HomeController {
 	public String tomorrow() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
 	}
-	
-	@RequestMapping("/daikichi/travel/{location}")
+
+	//START OF DAIKICHI PATH VARIABLES
+	@RequestMapping(value = "/daikichi/travel", method = RequestMethod.GET)
+	public String travel() {
+		return "You will soon take a trip and meet someone who will change your life!";
+	}
+
+	@RequestMapping(value = "/daikichi/travel/{location}", method = RequestMethod.GET)
 	public String travelToLocation(@PathVariable("location") String location) {
-		if (location == null) {
-			return "You will soon take a trip and meet someone who will change your life!";
-		}
 		return "Congratulations! You will soon travel to " + location + "!";
 	}
 	
+	@RequestMapping(value = "/daikichi/lotto/{num}", method = RequestMethod.GET)
+	public String lotto(@PathVariable("num") int num) {
+		if (num % 2 == 0) {
+			return "You will take a grand journey in the near future, but be weary of tempting offers.";
+		}
+		else {
+			return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
+		}
+	}
 }
